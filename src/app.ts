5dd4e08b236cc7044209680ef703d6b0000c33a1
Matthias <tom4tto@outlook.com>
2023-03-12 23:02:54 +0800

    load up all chemical models & animation

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,8 @@ export class App {
     private data: AuthoringData /** Authoring data from  XRAuthor */
     private videoPlane: Mesh /** Plane to render video on */
 
+    public modelIDs: string[] = ["m3", "m4", "m6", "m9", "m11"]
+
     //TEMP GLOBALS
     private animationGroup: AnimationGroup
 
@@ -123,51 +125,53 @@ export class App {
         // sphere.position.y = 1;
         // sphere.position.z = 5;
 
-        const id = "m3"
-        const track = this.data.recordingData.animation.tracks[id]
-        //convert A-Frame animation (matrices and time) used in XRAuthor to BabylonJS (frame idx)
-        const length = track.times.length //how many frames
-        const fps = length / this.data.recordingData.animation.duration
-        //babylon js doesnt manipulate matrices directly, if we want to manipulate the pos/scale/rot, we need to extract them from the matrix and get the vector if we need them
-        const keyframes: { frame: number; value: Vector3; }[] = [];
-        for (let i = 0; i < length; i++) {
-            //1 matrix 1 frame, stored in a json file in a simple array by Prof in XRAuthor
-            const mat = Matrix.FromArray(track.matrices[i].elements)
-            const position = mat.getTranslation()
-            position.z = -position.z //convert position from Right handed (AFrame) to Left Handed (babylonjs)
-            //moving the animation from the recorded z to the video plane's z in the current scene
-            const s = this.videoPlane.position.z / position.z //desired depth / depth
-            keyframes.push({
-                //time * fps = frame idx
-                frame: track.times[i] * fps, //gets you frame idx
-                //different sizes of the video planes used in the xrauthor scene and the babylonjs scene's videoPlane, need rescale
-                value: position.scale(s).multiplyByFloats(3, 3, 1)
+        for (const id of this.modelIDs) {
+            const track = this.data.recordingData.animation.tracks[id]
+            //convert A-Frame animation (matrices and time) used in XRAuthor to BabylonJS (frame idx)
+            const length = track.times.length //how many frames
+            const fps = length / this.data.recordingData.animation.duration
+            //babylon js doesnt manipulate matrices directly, if we want to manipulate the pos/scale/rot, we need to extract them from the matrix and get the vector if we need them
+            const keyframes: { frame: number; value: Vector3; }[] = [];
+            for (let i = 0; i < length; i++) {
+                //1 matrix 1 frame, stored in a json file in a simple array by Prof in XRAuthor
+                const mat = Matrix.FromArray(track.matrices[i].elements)
+                const position = mat.getTranslation()
+                position.z = -position.z //convert position from Right handed (AFrame) to Left Handed (babylonjs)
+                //moving the animation from the recorded z to the video plane's z in the current scene
+                const s = this.videoPlane.position.z / position.z //desired depth / depth
+                keyframes.push({
+                    //time * fps = frame idx
+                    frame: track.times[i] * fps, //gets you frame idx
+                    //different sizes of the video planes used in the xrauthor scene and the babylonjs scene's videoPlane, need rescale
+                    value: position.scale(s).multiplyByFloats(3, 3, 1)
+                })
+            }
+            const animation = new Animation("animation", "position", fps, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CYCLE)
+            animation.setKeys(keyframes)
+            //sphere.animations = [animation]
+            //scene.beginAnimation(sphere, 0, length - 1, true)
+            //Create animation group instead, for control over multiple models
+            this.animationGroup = new AnimationGroup("animation group", scene)
+            //Animating our video models
+            const info = this.data.recordingData.modelInfo[id]
+            const label = info.label
+            const name = info.name
+            const url = this.data.models[name]
+            //dont need ImportMesh as its already loaded from XRAuthor authoring data, load from url
+            SceneLoader.AppendAsync(url, undefined, scene, undefined, ".glb").then(result => {
+                //glb or gltf models, BabylonJS will add a root object to model
+                const root = result.getMeshById("__root__")
+                root.id = id + ": " + label //make a unique ID instead of everybody sharing root
+                root.name = label
+
+                //generate label text a bit below and behind model
+                const labelPlane = new TextPlane(label, "purple", 50, root.id, 2.5, 1, 0, -0.5, -0.1, "", scene, root)
+                this.animationGroup.addTargetedAnimation(animation, root)
+                //init starting pos
+                this.animationGroup.reset() //reset to first frame
             })
         }
-        const animation = new Animation("animation", "position", fps, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CYCLE)
-        animation.setKeys(keyframes)
-        //sphere.animations = [animation]
-        //scene.beginAnimation(sphere, 0, length - 1, true)
-        //Create animation group instead, for control over multiple models
-        this.animationGroup = new AnimationGroup("animation group", scene)
-        //Animating our video models
-        const info = this.data.recordingData.modelInfo[id]
-        const label = info.label
-        const name = info.name
-        const url = this.data.models[name]
-        //dont need ImportMesh as its already loaded from XRAuthor authoring data, load from url
-        SceneLoader.AppendAsync(url, undefined, scene, undefined, ".glb").then(result => {
-            //glb or gltf models, BabylonJS will add a root object to model
-            const root = result.getMeshById("__root__")
-            root.id = id + ": " + label //make a unique ID instead of everybody sharing root
-            root.name = label
 
-            //generate label text a bit below and behind model
-            const labelPlane = new TextPlane(label, "purple", 50, root.id, 2.5, 1, 0, -0.5, -0.1, "", scene, root)
-            this.animationGroup.addTargetedAnimation(animation, root)
-            //init starting pos
-            this.animationGroup.reset() //reset to first frame
-        })
     }
 
     createCamera(scene: Scene) {
