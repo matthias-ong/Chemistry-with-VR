6ecbec187caac4ccc75d6da618a4c23930b571bb
chektien <chek@gamesstudio.org>
2023-04-05 21:42:56 +0800

    Added your ASSESSMENT in ipa-b-feedback.md

---
+++ ipa-b-feedback.md
@@ -0,0 +1,28 @@
+# IPA-B Feedback for MATTHIAS ONG SI EN
+                                        
+**OVERALL IPA-A + IPA-B Grade**: A
+
+**IPA-B Grade**: B+
+                                        
+## B1
+complete 3D scene (with (a) a background environment or live video feed and (b) manipulable objects) and clear consistent theme
+                                        
+## B2
+desktop-based grab and translate interaction exists and works well
+                                        
+## B3
+both desktop and an additional form of attach interaction exist and works well
+                                        
+## B4
+desktop-based rotate interaction exists but very buggy (or you only had additional, e.g. HMD, interaction but did not implement desktop-based)
+                                        
+## B5
+desktop-based "pinch" interaction exists but very buggy (or you only had additional, e.g. HMD, interaction but did not implement desktop-based)
+                                        
+## B6
+both desktop and an additional form of locomotion interaction exist and works well
+                                        
+## Other feedback
+decent interactions but very buggy, e.g., rendering is extremely choppy
+double-click gizmos rather buggy in desktop and doesn't trigger in HMD
+the interactions did not cater for the HMD experience
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,33 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const CopyPlugin = require("copy-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.ts'),
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist/app')
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    },
-    mode: "development",
-    devtool: 'inline-source-map', //see error msges (optional)
-    devServer: {
-        //static files e.g. non code, assets by default is true, you can specify a path though
-        //static: true, 
-        port: 3001,
-        server: "https"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, 'public') }
-            ]
-        })
-    ]
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist/app"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+  mode: "development",
+  devtool: "inline-source-map", //see error msges (optional)
+  devServer: {
+    //static files e.g. non code, assets by default is true, you can specify a path though
+    //static: true,
+    port: 3001,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: path.resolve(__dirname, "public") }],
+    }),
+  ],
 };
+
